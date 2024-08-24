405c97288d8d7c5d635a93ec095b6c8534049e35
MauricioSalvador <mauriciosalvadorcastro@gmail.com>
2024-08-24 13:30:01 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import "./index.css";
+import '@fontsource-variable/raleway';
+
+function App() {
+  return <div className="text-blue-500">causa</div>;
+}
+
+export default App;
